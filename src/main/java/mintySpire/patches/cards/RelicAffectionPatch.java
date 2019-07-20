975f77b6380b3632b450cbe23a4917b0fe40e657
erasels <emanuelbedburdick@yahoo.de>
2019-07-20 19:14:58 +0200

    Multi display fo Relic affection

--- src/main/java/mintySpire/patches/cards/RelicAffectionPatch.java
+++ src/main/java/mintySpire/patches/cards/RelicAffectionPatch.java
@@ -19,7 +19,7 @@ import javassist.CtBehavior;
 
 import java.util.ArrayList;
 
-public class DoublePlayBorderColorPatch {
+public class RelicAffectionPatch {
     @SpirePatch(clz = AbstractCard.class, method=SpirePatch.CLASS)
     public static class cardFields {
         public static SpireField<Boolean> isNecroAff = new SpireField<>(() -> false);
@@ -35,23 +35,25 @@ public class DoublePlayBorderColorPatch {
             if(shouldChangeGlow(c)) {
                 Color color = Color.PURPLE.cpy();
                 ReflectionHacks.setPrivate(__instance, AbstractGameEffect.class, "color", color);
-            } else {
-                cardFields.isNecroAff.set(c, false);
-                cardFields.isPenAff.set(c, false);
             }
         }
     }
 
     public static boolean shouldChangeGlow(AbstractCard c) {
         if(CardCrawlGame.isInARun()) {
+            boolean isAffected = false;
             if (c.type == AbstractCard.CardType.ATTACK) {
                 //Pennib
                 PenNib pn = (PenNib) AbstractDungeon.player.getRelic(PenNib.ID);
                 if (pn != null) {
                     if (pn.counter == PenNib.COUNT - 1) {
                         cardFields.isPenAff.set(c, true);
-                        return true;
+                        isAffected = true;
+                    } else {
+                        cardFields.isPenAff.set(c, false);
                     }
+                } else {
+                    cardFields.isPenAff.set(c, false);
                 }
 
                 //Necro
@@ -60,11 +62,19 @@ public class DoublePlayBorderColorPatch {
                     if (nm != null) {
                         if (nm.checkTrigger()) {
                             cardFields.isNecroAff.set(c, true);
-                            return true;
+                            isAffected = true;
+                        } else {
+                            cardFields.isNecroAff.set(c, false);
                         }
                     }
+                } else {
+                    cardFields.isNecroAff.set(c, false);
                 }
+            } else {
+                cardFields.isNecroAff.set(c, false);
+                cardFields.isPenAff.set(c, false);
             }
+            return isAffected;
         }
         return false;
     }
@@ -72,19 +82,24 @@ public class DoublePlayBorderColorPatch {
     @SpirePatch(clz = AbstractCard.class, method = "renderCard")
     public static class RenderPatch {
         public static void Postfix(AbstractCard card, SpriteBatch sb, boolean b1, boolean b2) {
+            int numRelics = 0;
             if (cardFields.isNecroAff.get(card)) {
                 nCon.currentX = card.current_x + 390.0f * card.drawScale / 3.0f * Settings.scale;
                 nCon.currentY = card.current_y + 546.0f * card.drawScale / 3.0f * Settings.scale;
                 nCon.scale = card.drawScale;
                 nCon.renderOutline(sb, false);
                 nCon.render(sb);
-            } else if (cardFields.isPenAff.get(card)) {
+                numRelics++;
+            }
+
+            if (cardFields.isPenAff.get(card)) {
                 pNib.counter = -1;
-                pNib.currentX = card.current_x + 390.0f * card.drawScale / 3.0f * Settings.scale;
+                pNib.currentX = card.current_x + (390.0f + (numRelics*pNib.img.getWidth())) * card.drawScale / 3.0f * Settings.scale;
                 pNib.currentY = card.current_y + 546.0f * card.drawScale / 3.0f * Settings.scale;
                 pNib.scale = card.drawScale;
                 pNib.renderOutline(sb, false);
                 pNib.render(sb);
+                //numRelics++;
             }
         }
     }
