474c96a976324b212a177fbfe379934930e69df6
erasels <emanuelbedburdick@yahoo.de>
2019-09-27 12:57:41 +0200

    Create RemoveSplashPatches.java

---
+++ src/main/java/mintySpire/patches/Splashscreen/RemoveSplashPatches.java
@@ -0,0 +1,16 @@
+package mintySpire.patches.Splashscreen;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import com.megacrit.cardcrawl.screens.splash.SplashScreen;
+
+@SpirePatch(
+        clz = SplashScreen.class,
+        method = "update"
+)
+public class RemoveSplashPatches {
+    public static SpireReturn Prefix(SplashScreen __instance) {
+        __instance.isDone = true;
+        return SpireReturn.Return(null);
+    }
+}
