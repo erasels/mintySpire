78ad5d6ad549501f54879001c53c87da306c5945
erasels <emanuelbedburdick@yahoo.de>
2019-10-08 09:14:44 +0200

    Create MultiAttackIntentSumPatches.java

---
+++ src/main/java/mintySpire/patches/monsters/MultiAttackIntentSumPatches.java
@@ -0,0 +1,28 @@
+package mintySpire.patches.monsters;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.helpers.FontHelper;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import javassist.CannotCompileException;
+import javassist.expr.ExprEditor;
+import javassist.expr.MethodCall;
+
+public class MultiAttackIntentSumPatches {
+    private static boolean firstRun = true;
+    @SpirePatch(clz = AbstractMonster.class, method = "renderDamageRange")
+    public static class TextRender {
+        public static ExprEditor Instrument() {
+            return new ExprEditor() {
+                @Override
+                public void edit(MethodCall m) throws CannotCompileException {
+                    if (m.getClassName().equals(FontHelper.class.getName()) && m.getMethodName().equals("renderFontLeftTopAligned") && firstRun) {
+                        firstRun = false;
+                        m.replace("{" +
+                                FontHelper.class.getName()+".renderFontLeftTopAligned($1, $2, $3 + \" (\" + intentDmg*intentMultiAmt + \")\", $4, $5, $6);" +
+                                "}");
+                    }
+                }
+            };
+        }
+    }
+}
