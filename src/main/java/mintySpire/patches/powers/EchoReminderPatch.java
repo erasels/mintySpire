712d21c92f165ec766c10ea03dce0e25fd633866
Linzi Zhu <linzi.zhu@hotmail.com>
2020-11-13 13:55:38 +1100

    Split the conditional check into it's own function, for easier readability and management

--- src/main/java/mintySpire/patches/powers/EchoReminderPatch.java
+++ src/main/java/mintySpire/patches/powers/EchoReminderPatch.java
@@ -22,8 +22,7 @@ public class EchoReminderPatch {
     private static TextureAtlas.AtlasRegion doubleRegion = new TextureAtlas.AtlasRegion(doubleTexture, 0, 0, doubleTexture.getWidth(), doubleTexture.getHeight());
 
     public static void Postfix(AbstractCard __instance, SpriteBatch sb){
-            if(MintySpire.showEFR() && AbstractDungeon.player != null && AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT && !AbstractDungeon.player.drawPile.contains(__instance) && (AbstractDungeon.player.hasPower(EchoPower.POWER_ID)) &&
-                    (!__instance.purgeOnUse && AbstractDungeon.player.getPower(EchoPower.POWER_ID).amount > 0 && AbstractDungeon.actionManager.cardsPlayedThisTurn.size() - (int)ReflectionHacks.getPrivate(AbstractDungeon.player.getPower(EchoPower.POWER_ID), EchoPower.class, "cardsDoubledThisTurn") < AbstractDungeon.player.getPower(EchoPower.POWER_ID).amount)){
+            if(echoFormValidChecker(__instance)) {
             renderHelper(sb, doubleRegion, __instance.current_x, __instance.current_y, __instance);
         }
     }
@@ -31,6 +30,16 @@ public class EchoReminderPatch {
     private static void renderHelper(SpriteBatch sb, TextureAtlas.AtlasRegion img, float drawX, float drawY, AbstractCard C){
         sb.setColor(Color.WHITE);
         sb.draw(img, drawX + img.offsetX - (float) img.originalWidth / 2.0F, drawY + img.offsetY - (float) img.originalHeight / 2.0F, (float) img.originalWidth / 2.0F - img.offsetX, (float) img.originalHeight / 2.0F - img.offsetY, (float) img.packedWidth, (float) img.packedHeight, C.drawScale * Settings.scale, C.drawScale * Settings.scale, C.angle);
+    }
+
+    private static boolean echoFormValidChecker(AbstractCard __instance){
+        return (MintySpire.showEFR() &&
+                AbstractDungeon.player != null &&
+                AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT &&
+                !AbstractDungeon.player.drawPile.contains(__instance) &&
+                (AbstractDungeon.player.hasPower(EchoPower.POWER_ID)) &&
+                !__instance.purgeOnUse && AbstractDungeon.player.getPower(EchoPower.POWER_ID).amount > 0 &&
+                AbstractDungeon.actionManager.cardsPlayedThisTurn.size() - (int)ReflectionHacks.getPrivate(AbstractDungeon.player.getPower(EchoPower.POWER_ID), EchoPower.class, "cardsDoubledThisTurn") < AbstractDungeon.player.getPower(EchoPower.POWER_ID).amount);
     }
 
 }
