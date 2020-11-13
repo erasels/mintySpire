316874a8b687271575a557d257c4ef8d10049f03
Linzi Zhu <linzi.zhu@hotmail.com>
2020-11-13 15:47:11 +1100

    Used the glow effect from Spicy Shops to give some more flavour to the x2

--- src/main/java/mintySpire/patches/powers/EchoReminderPatch.java
+++ src/main/java/mintySpire/patches/powers/EchoReminderPatch.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.MathUtils;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.Settings;
@@ -22,13 +23,18 @@ public class EchoReminderPatch {
     private static TextureAtlas.AtlasRegion doubleRegion = new TextureAtlas.AtlasRegion(doubleTexture, 0, 0, doubleTexture.getWidth(), doubleTexture.getHeight());
 
     public static void Postfix(AbstractCard __instance, SpriteBatch sb){
-            if(echoFormValidChecker(__instance)) {
+        if(echoFormValidChecker(__instance)) {
+            sb.setColor(Color.WHITE);
             renderHelper(sb, doubleRegion, __instance.current_x, __instance.current_y, __instance);
+            sb.setBlendFunction(770, 1);
+            sb.setColor(new Color(1.0F, 1.0F, 1.0F, (MathUtils.cosDeg((float) (System.currentTimeMillis() / 5L % 360L)) + 1.25F) / 3.0F));
+            renderHelper(sb, doubleRegion, __instance.current_x, __instance.current_y, __instance);
+            sb.setBlendFunction(770, 771);
+            sb.setColor(Color.WHITE);
         }
     }
 
     private static void renderHelper(SpriteBatch sb, TextureAtlas.AtlasRegion img, float drawX, float drawY, AbstractCard C){
-        sb.setColor(Color.WHITE);
         sb.draw(img, drawX + img.offsetX - (float) img.originalWidth / 2.0F, drawY + img.offsetY - (float) img.originalHeight / 2.0F, (float) img.originalWidth / 2.0F - img.offsetX, (float) img.originalHeight / 2.0F - img.offsetY, (float) img.packedWidth, (float) img.packedHeight, C.drawScale * Settings.scale, C.drawScale * Settings.scale, C.angle);
     }
 
